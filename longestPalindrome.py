457719377657c59d3fd1eb7b70bc4c55cc82fb44
Maoning Guan <1812711281@qq.com>
2020-07-28 17:25:07 +0800

    update questions.

---
+++ longestPalindrome.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
+
+示例 1：
+
+输入: "babad"
+输出: "bab"
+注意: "aba" 也是一个有效答案。
+示例 2：
+
+输入: "cbbd"
+输出: "bb"
+"""
+
+
+class Solution:
+    def longestPalindrome_dp(self, s: str) -> str:
+        """
+        动态规划算法：自底向上解法
+        :param s:
+        :return:
+        """
+        n = len(s)
+        dp = [[False] * n for _ in range(n)]
+        ans = ""
+        # 枚举子串的长度 l+1
+        for l in range(n):
+            # 枚举子串的起始位置 i，这样可以通过 j=i+l 得到子串的结束位置
+            for i in range(n):
+                j = i + l
+                if j >= len(s):
+                    break
+                if l == 0:
+                    dp[i][j] = True
+                elif l == 1:
+                    dp[i][j] = (s[i] == s[j])
+                else:
+                    dp[i][j] = (dp[i + 1][j - 1] and s[i] == s[j])
+                if dp[i][j] and l + 1 > len(ans):
+                    ans = s[i:j + 1]
+        return ans
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    str_test = 'babad'
+    print(solution.longestPalindrome_dp(str_test))
