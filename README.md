0170e82a2f3717d54722dab7e60a6cc6722d4068
Maoning Guan <1812711281@qq.com>
2020-07-30 11:14:13 +0800

    添加：LRU缓存机制python代码

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ LeetCode（力扣）算法编程
 * lengthOfLongestSubstring.py：无重复字符的最长子串
 * findMedianSortedArrays.py：寻找两个正序数组的中位数  
 * longestPalindrome.py：最长回文子串 
+* LRUCache.py：LRU缓存机制
--- longestPalindrome.py
+++ longestPalindrome.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 """
+最长回文子串:
 给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
 
 示例 1：
@@ -18,8 +19,10 @@ class Solution:
     def longestPalindrome_dp(self, s: str) -> str:
         """
         动态规划算法：自底向上解法
-        :param s:
-        :return:
+        算法时间复杂度：O(n^2)
+        算法空间复杂度：O(n^2)
+        :param s:目标字符串
+        :return:最长回文子串
         """
         n = len(s)
         dp = [[False] * n for _ in range(n)]
@@ -27,22 +30,47 @@ class Solution:
         # 枚举子串的长度 l+1
         for l in range(n):
             # 枚举子串的起始位置 i，这样可以通过 j=i+l 得到子串的结束位置
-            for i in range(n):
+            for i in range(n):  # 从第一个字符开始枚举
                 j = i + l
                 if j >= len(s):
                     break
-                if l == 0:
+                if l == 0:  # 单个字符的子串
                     dp[i][j] = True
-                elif l == 1:
+                elif l == 1:  # 两个字符的子串
                     dp[i][j] = (s[i] == s[j])
-                else:
-                    dp[i][j] = (dp[i + 1][j - 1] and s[i] == s[j])
-                if dp[i][j] and l + 1 > len(ans):
-                    ans = s[i:j + 1]
+                else:  # 三个以上字符的子串
+                    dp[i][j] = (dp[i + 1][j - 1] and s[i] == s[j])  # 动态规划的状态转移方程
+                if dp[i][j] and l + 1 > len(ans):  # 子串的长度为 l + 1
+                    ans = s[i:j + 1]  # 要取s[i:j + 1]，因为Python切片不包括j + 1位置的元素
         return ans
+
+    def longestPalindrome_expandAroundCenter(self, s: str) -> str:
+        """
+        中心扩展算法，
+        算法时间复杂度：O(n^2)
+        算法空间复杂度：O(1)
+        :param s:
+        :return:
+        """
+        def expandAroundCenter(s, left, right):
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                left -= 1
+                right += 1
+            return left + 1, right - 1
+
+        start, end = 0, 0
+        for i in range(len(s)):
+            left1, right1 = expandAroundCenter(s, i, i)
+            left2, right2 = expandAroundCenter(s, i, i + 1)
+            if right1 - left1 > end - start:
+                start, end = left1, right1
+            if right2 - left2 > end - start:
+                start, end = left2, right2
+        return s[start: end + 1]
+
 
 
 if __name__ == '__main__':
     solution = Solution()
-    str_test = 'babad'
-    print(solution.longestPalindrome_dp(str_test))
+    str_test = 'babaddaba'
+    print(solution.longestPalindrome_expandAroundCenter(str_test))
