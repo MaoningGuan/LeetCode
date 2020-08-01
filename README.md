77111814359a11c57c3a5c390a911e7bcf0a7798
Maoning Guan <1812711281@qq.com>
2020-08-01 21:47:22 +0800

    更新：84. 柱状图中最大的矩形

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # LeetCode
 LeetCode（力扣）算法编程
-* twoSum.py：两数之和
-* addTwoNumbers.py：两数相加
-* lengthOfLongestSubstring.py：无重复字符的最长子串
-* findMedianSortedArrays.py：寻找两个正序数组的中位数  
-* longestPalindrome.py：最长回文子串 
-* LRUCache.py：LRU缓存机制
-* reverseList.py：反转链表
-* trap.py：接雨水
+1. twoSum.py：1. 两数之和
+2. addTwoNumbers.py：2. 两数相加
+3. lengthOfLongestSubstring.py：3. 无重复字符的最长子串
+4. findMedianSortedArrays.py：4. 寻找两个正序数组的中位数  
+5. longestPalindrome.py：5. 最长回文子串 
+6. LRUCache.py：146. LRU缓存机制
+7.  reverseList.py：206. 反转链表
+8.  trap.py：42. 接雨水
+9. largestRectangleArea：84. 柱状图中最大的矩形
---
+++ largestRectangleArea.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+"""
+给定 n 个非负整数，用来表示柱状图中各个柱子的高度。每个柱子彼此相邻，且宽度为 1 。
+
+求在该柱状图中，能够勾勒出来的矩形的最大面积。
+示例:
+
+输入: [2,1,5,6,2,3]
+输出: 10
+"""
+from typing import List
+
+
+class Solution:
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        """
+        方法一：单调栈
+        时间复杂度：O(N)O(N)。
+        空间复杂度：O(N)O(N)。
+        :param heights:
+        :return:
+        """
+        n = len(heights)
+        left, right = [0] * n, [0] * n
+
+        mono_stack = list()
+        for i in range(n):
+            while mono_stack and heights[mono_stack[-1]] >= heights[i]:
+                mono_stack.pop()
+            left[i] = mono_stack[-1] if mono_stack else -1
+            mono_stack.append(i)
+
+        mono_stack = list()
+        for i in range(n - 1, -1, -1):
+            while mono_stack and heights[mono_stack[-1]] >= heights[i]:
+                mono_stack.pop()
+            right[i] = mono_stack[-1] if mono_stack else n
+            mono_stack.append(i)
+
+        # 因为不包括左右边界的柱子：(right[i] - left[i] - 1)
+        ans = max((right[i] - left[i] - 1) * heights[i] for i in range(n)) if n > 0 else 0
+        return ans
+
+    def largestRectangleArea_optimize(self, heights: List[int]) -> int:
+        """
+        方法二：单调栈 + 常数优化
+        时间复杂度：O(N)O(N)。
+        空间复杂度：O(N)O(N)。
+        :param heights:
+        :return:
+        """
+        n = len(heights)
+        left, right = [0] * n, [n] * n
+
+        mono_stack = list()
+        for i in range(n):
+            while mono_stack and heights[mono_stack[-1]] >= heights[i]:
+                right[mono_stack[-1]] = i
+                mono_stack.pop()
+            left[i] = mono_stack[-1] if mono_stack else -1
+            mono_stack.append(i)
+
+        ans = max((right[i] - left[i] - 1) * heights[i] for i in range(n)) if n > 0 else 0
+        return ans
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    heights = [2, 1, 5, 6, 2, 3]
+    print(solution.largestRectangleArea(heights))
+    print(solution.largestRectangleArea_optimize(heights))
