7f1695beaeb161f2c0899ca47234dafe6c63fcf6
Maoning Guan <1812711281@qq.com>
2020-08-24 15:37:39 +0800

    添加：剑指 Offer 41. 数据流中的中位数

---
+++ 剑指 Offer（第 2 版）/MedianFinder.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+剑指 Offer 41. 数据流中的中位数
+如何得到一个数据流中的中位数？如果从数据流中读出奇数个数值，那么中位数就是所有数值排序之后位于中间的数值。如果从数据流中读出偶数个数值，那么中位数就是所有数值排序之后中间两个数的平均值。
+
+例如，
+
+[2,3,4] 的中位数是 3
+
+[2,3] 的中位数是 (2 + 3) / 2 = 2.5
+
+设计一个支持以下两种操作的数据结构：
+
+void addNum(int num) - 从数据流中添加一个整数到数据结构中。
+double findMedian() - 返回目前所有元素的中位数。
+示例 1：
+
+输入：
+["MedianFinder","addNum","addNum","findMedian","addNum","findMedian"]
+[[],[1],[2],[],[3],[]]
+输出：[null,null,null,1.50000,null,2.00000]
+示例 2：
+
+输入：
+["MedianFinder","addNum","findMedian","addNum","findMedian"]
+[[],[2],[],[3],[]]
+输出：[null,null,2.00000,null,2.50000]
+"""
+from heapq import *
+
+
+class MedianFinder:
+    def __init__(self):
+        self.A = []  # 小顶堆，保存较大的一半
+        self.B = []  # 大顶堆，保存较小的一半
+
+    def addNum(self, num: int) -> None:
+        if len(self.A) != len(self.B):
+            heappush(self.B, -heappushpop(self.A, num))
+        else:
+            heappush(self.A, -heappushpop(self.B, -num))
+
+    def findMedian(self) -> float:
+        return self.A[0] if len(self.A) != len(self.B) else (self.A[0] - self.B[0]) / 2.0
+
+
+if __name__ == '__main__':
+    obj = MedianFinder()
+    obj.addNum(4)
+    print(obj.findMedian())
+    obj.addNum(2)
+    print(obj.findMedian())
+    obj.addNum(22)
+    print(obj.findMedian())
