f3a28a0c18c4e0c14ed2810d639b5334eff07f5b
Maoning Guan <1812711281@qq.com>
2020-09-09 17:13:08 +0800

    添加：在排序数组中查找元素的第一个和最后一个位置

---
+++ 软件开发岗刷题（华为笔试准备）/二分查找/searchRange.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+在排序数组中查找元素的第一个和最后一个位置
+给定一个按照升序排列的整数数组 nums，和一个目标值 target。找出给定目标值在数组中的开始位置和结束位置。
+
+你的算法时间复杂度必须是 O(log n) 级别。
+
+如果数组中不存在目标值，返回 [-1, -1]。
+
+示例 1:
+
+输入: nums = [5,7,7,8,8,10], target = 8
+输出: [3,4]
+示例 2:
+
+输入: nums = [5,7,7,8,8,10], target = 6
+输出: [-1,-1]
+"""
+
+
+class Solution:
+    # returns leftmost (or rightmost) index at which `target` should be inserted in sorted
+    # array `nums` via binary search.
+    def extreme_insertion_index(self, nums, target, left):
+        lo = 0
+        hi = len(nums)
+
+        while lo < hi:
+            mid = (lo + hi) // 2
+            if nums[mid] > target or (left and target == nums[mid]):
+                hi = mid
+            else:
+                lo = mid+1
+
+        return lo
+
+
+    def searchRange(self, nums, target):
+        left_idx = self.extreme_insertion_index(nums, target, True)
+
+        # assert that `left_idx` is within the array bounds and that `target`
+        # is actually in `nums`.
+        if left_idx == len(nums) or nums[left_idx] != target:
+            return [-1, -1]
+
+        return [left_idx, self.extreme_insertion_index(nums, target, False)-1]
+
+
+
+if __name__ == '__main__':
+    nums = [5, 7, 7, 8, 8, 10]
+    target = 8
+
+    # nums = [5, 7, 7, 8, 8, 10]
+    # target = 6
+
+    solution = Solution()
+    print(solution.searchRange(nums, target))
