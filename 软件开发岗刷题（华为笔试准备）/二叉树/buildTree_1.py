d6f2837fffb087127e6cccab9cf8826d6069fdbf
Maoning Guan <1812711281@qq.com>
2020-09-11 09:44:46 +0800

    添加：106. 从中序与后序遍历序列构造二叉树

---
+++ 软件开发岗刷题（华为笔试准备）/二叉树/buildTree_1.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+"""
+106. 从中序与后序遍历序列构造二叉树
+根据一棵树的中序遍历与后序遍历构造二叉树。
+
+注意:
+你可以假设树中没有重复的元素。
+
+例如，给出
+
+中序遍历 inorder = [9,3,15,20,7]
+后序遍历 postorder = [9,15,7,20,3]
+返回如下的二叉树：
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+"""
+from typing import List
+
+from utils import printTree
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def buildTree(self, inorder: List[int], postorder: List[int]) -> TreeNode:
+        def helper(in_left, in_right):
+            # if there is no elements to construct subtrees
+            if in_left > in_right:
+                return None
+
+            # pick up the last element as a root
+            val = postorder.pop()
+            root = TreeNode(val)
+
+            # root splits inorder list
+            # into left and right subtrees
+            index = idx_map[val]
+
+            # build right subtree
+            root.right = helper(index + 1, in_right)
+            # build left subtree
+            root.left = helper(in_left, index - 1)
+            return root
+
+        # build a hashmap value -> its index
+        idx_map = {val: idx for idx, val in enumerate(inorder)}
+        return helper(0, len(inorder) - 1)
+
+
+if __name__ == '__main__':
+    inorder = [9, 3, 15, 20, 7]
+    postorder = [9, 15, 7, 20, 3]
+
+    solution = Solution()
+    root = solution.buildTree(inorder, postorder)
+
+    printTree(root)
