bd1c126b868bee2ebea6c509811b66bece5ed136
Maoning Guan <1812711281@qq.com>
2020-09-12 16:36:38 +0800

    622. 设计循环队列

---
+++ 软件开发岗刷题（华为笔试准备）/队列和栈/CircularQueue_2.py
@@ -0,0 +1,109 @@
+# -*- coding: utf-8 -*-
+"""
+622. 设计循环队列
+设计你的循环队列实现。 循环队列是一种线性数据结构，其操作表现基于 FIFO（先进先出）原则
+并且队尾被连接在队首之后以形成一个循环。它也被称为“环形缓冲器”。
+
+循环队列的一个好处是我们可以利用这个队列之前用过的空间。在一个普通队列里，一旦一个队列满了，我们就不能插入下一个元素，
+即使在队列前面仍有空间。但是使用循环队列，我们能使用这些空间去存储新的值。
+
+你的实现应该支持如下操作：
+
+MyCircularQueue(k): 构造器，设置队列长度为 k 。
+Front: 从队首获取元素。如果队列为空，返回 -1 。
+Rear: 获取队尾元素。如果队列为空，返回 -1 。
+enQueue(value): 向循环队列插入一个元素。如果成功插入则返回真。
+deQueue(): 从循环队列中删除一个元素。如果成功删除则返回真。
+isEmpty(): 检查循环队列是否为空。
+isFull(): 检查循环队列是否已满。
+"""
+
+
+# ===================单链表实现队列====================
+class Node:
+    def __init__(self, value, nextNode=None):
+        self.value = value
+        self.next = nextNode
+
+
+class MyCircularQueue:
+
+    def __init__(self, k: int):
+        """
+        Initialize your data structure here. Set the size of the queue to be k.
+        """
+        self.capacity = k
+        self.head = None
+        self.tail = None
+        self.count = 0
+
+    def enQueue(self, value: int) -> bool:
+        """
+        Insert an element into the circular queue. Return true if the operation is successful.
+        """
+        if self.count == self.capacity:
+            return False
+
+        if self.count == 0:
+            self.head = Node(value)
+            self.tail = self.head
+        else:
+            newNode = Node(value)
+            self.tail.next = newNode
+            self.tail = newNode
+        self.count += 1
+        return True
+
+    def deQueue(self) -> bool:
+        """
+        Delete an element from the circular queue. Return true if the operation is successful.
+        """
+        if self.count == 0:
+            return False
+        self.head = self.head.next
+        self.count -= 1
+        return True
+
+    def Front(self) -> int:
+        """
+        Get the front item from the queue.
+        """
+        if self.count == 0:
+            return -1
+        return self.head.value
+
+    def Rear(self) -> int:
+        """
+        Get the last item from the queue.
+        """
+        # empty queue
+        if self.count == 0:
+            return -1
+        return self.tail.value
+
+    def isEmpty(self) -> bool:
+        """
+        Checks whether the circular queue is empty or not.
+        """
+        return self.count == 0
+
+    def isFull(self) -> bool:
+        """
+        Checks whether the circular queue is full or not.
+        """
+        return self.count == self.capacity
+
+
+if __name__ == '__main__':
+    # Your MyCircularQueue object will be instantiated and called as such:
+
+    circularQueue = MyCircularQueue(3)  # 设置长度为 3
+    print(circularQueue.enQueue(1))  # 返回 true
+    print(circularQueue.enQueue(2))  # 返回 true
+    print(circularQueue.enQueue(3))  # 返回 true
+    print(circularQueue.enQueue(4))  # 返回 false，队列已满
+    print(circularQueue.Rear())  # 返回 3
+    print(circularQueue.isFull())  # 返回 true
+    print(circularQueue.deQueue())  # 返回 true
+    print(circularQueue.enQueue(4))  # 返回 true
+    print(circularQueue.Rear())  # 返回 4
