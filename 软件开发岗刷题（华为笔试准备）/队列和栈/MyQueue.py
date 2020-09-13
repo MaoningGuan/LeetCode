5bfd93a6aaff8aaaa170a4593e6d9c1ee088e7e6
Maoning Guan <1812711281@qq.com>
2020-09-13 11:39:43 +0800

    添加：232. 用栈实现队列

---
+++ 软件开发岗刷题（华为笔试准备）/队列和栈/MyQueue.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+"""
+232. 用栈实现队列
+使用栈实现队列的下列操作：
+
+push(x) -- 将一个元素放入队列的尾部。
+pop() -- 从队列首部移除元素。
+peek() -- 返回队列首部的元素。
+empty() -- 返回队列是否为空。
+
+
+示例:
+
+MyQueue queue = new MyQueue();
+
+queue.push(1);
+queue.push(2);
+queue.peek();  // 返回 1
+queue.pop();   // 返回 1
+queue.empty(); // 返回 false
+
+
+说明:
+
+你只能使用标准的栈操作 -- 也就是只有 push to top, peek/pop from top, size, 和 is empty 操作是合法的。
+你所使用的语言也许不支持栈。你可以使用 list 或者 deque（双端队列）来模拟一个栈，只要是标准的栈操作即可。
+假设所有操作都是有效的 （例如，一个空的队列不会调用 pop 或者 peek 操作）。
+"""
+
+
+class MyQueue:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.s1 = []
+        self.s2 = []
+        self.front = None
+
+    def push(self, x: int) -> None:
+        """
+        Push element x to the back of queue.
+        """
+        if not self.s1: self.front = x
+        self.s1.append(x)
+
+    def pop(self) -> int:
+        """
+        Removes the element from in front of queue and returns that element.
+        """
+        if not self.s2:
+            while self.s1:
+                self.s2.append(self.s1.pop())
+            self.front = None
+        return self.s2.pop()
+
+    def peek(self) -> int:
+        """
+        Get the front element.
+        """
+        if self.s2:
+            return self.s2[-1]
+        return self.front
+
+    def empty(self) -> bool:
+        """
+        Returns whether the queue is empty.
+        """
+        if not self.s1 and not self.s2:
+            return True
+        return False
+
+
+if __name__ == '__main__':
+    obj = MyQueue()
+    obj.push(3)
+    print(obj.pop())
+    print(obj.peek())
+    print(obj.empty())
+    print(obj.peek())
