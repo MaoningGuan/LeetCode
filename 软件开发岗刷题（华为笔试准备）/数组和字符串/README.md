db28c22bafad16545523f7b0efa37e1b7ae1d468
Maoning Guan <1812711281@qq.com>
2020-09-15 20:24:40 +0800

    添加：35. 搜索插入位置

--- 软件开发岗刷题（华为笔试准备）/数组和字符串/README.md
+++ 软件开发岗刷题（华为笔试准备）/数组和字符串/README.md
@@ -1,2 +1,3 @@
 # LeetCode 软件开发岗刷题（华为笔试准备）——数组和字符串
 1. pivotIndex.py：724. 寻找数组的中心索引
+2. searchInsert.py：35. 搜索插入位置
---
+++ 软件开发岗刷题（华为笔试准备）/数组和字符串/searchInsert.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+35. 搜索插入位置
+给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+
+你可以假设数组中无重复元素。
+
+示例 1:
+
+输入: [1,3,5,6], 5
+输出: 2
+示例 2:
+
+输入: [1,3,5,6], 2
+输出: 1
+示例 3:
+
+输入: [1,3,5,6], 7
+输出: 4
+示例 4:
+
+输入: [1,3,5,6], 0
+输出: 0
+"""
+
+
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        for index, value in enumerate(nums):
+            if value == target or value > target:
+                return index
+        return len(nums)
+
+
+if __name__ == '__main__':
+    nums = [1, 3, 5, 6]
+    target = 7
+
+    solution = Solution()
+    print(solution.searchInsert(nums, target))
