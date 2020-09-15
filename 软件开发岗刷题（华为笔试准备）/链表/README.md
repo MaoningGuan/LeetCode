9daf3d28304b531b0633d73a13f4758f04939872
Maoning Guan <1812711281@qq.com>
2020-09-15 15:06:22 +0800

    添加：141. 环形链表

--- 软件开发岗刷题（华为笔试准备）/链表/README.md
+++ 软件开发岗刷题（华为笔试准备）/链表/README.md
@@ -1,3 +1,4 @@
 # LeetCode 软件开发岗刷题（华为笔试准备）——链表
 1. MyLinkedList_1.py：707. 设计链表（单链表）
 2. MyLinkedList_2.py：707. 设计链表（双链表）
+3. hasCycle.py：141. 环形链表
---
+++ 软件开发岗刷题（华为笔试准备）/链表/hasCycle.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+141. 环形链表
+给定一个链表，判断链表中是否有环。
+
+为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。
+
+
+
+示例 1：
+
+输入：head = [3,2,0,-4], pos = 1
+输出：true
+解释：链表中有一个环，其尾部连接到第二个节点。
+
+
+示例 2：
+
+输入：head = [1,2], pos = 0
+输出：true
+解释：链表中有一个环，其尾部连接到第一个节点。
+
+
+示例 3：
+
+输入：head = [1], pos = -1
+输出：false
+解释：链表中没有环。
+
+
+
+
+进阶：
+
+你能用 O(1)（即，常量）内存解决此问题吗？
+"""
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: ListNode) -> bool:
+        if head is None:
+            return False
+        fastnode = head
+        slownode = head
+        while (fastnode):
+            if fastnode.next and fastnode.next.next:
+                fastnode = fastnode.next.next
+                slownode = slownode.next
+            else:
+                return False
+
+            if fastnode == slownode:
+                return True
+        return False
+
