4b232dc7dc75e82cdf358c05f6ae70be5907e59d
Maoning Guan <1812711281@qq.com>
2020-09-15 15:29:12 +0800

    添加：142. 环形链表 II

--- 软件开发岗刷题（华为笔试准备）/链表/README.md
+++ 软件开发岗刷题（华为笔试准备）/链表/README.md
@@ -2,3 +2,4 @@
 1. MyLinkedList_1.py：707. 设计链表（单链表）
 2. MyLinkedList_2.py：707. 设计链表（双链表）
 3. hasCycle.py：141. 环形链表
+4. detectCycle.py：142. 环形链表 II
---
+++ 软件开发岗刷题（华为笔试准备）/链表/detectCycle.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+"""
+142. 环形链表 II
+给定一个链表，返回链表开始入环的第一个节点。 如果链表无环，则返回 null。
+
+为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。
+
+说明：不允许修改给定的链表。
+
+示例 1：
+
+输入：head = [3,2,0,-4], pos = 1
+输出：tail connects to node index 1
+解释：链表中有一个环，其尾部连接到第二个节点。
+
+示例 2：
+
+输入：head = [1,2], pos = 0
+输出：tail connects to node index 0
+解释：链表中有一个环，其尾部连接到第一个节点。
+
+示例 3：
+
+输入：head = [1], pos = -1
+输出：no cycle
+解释：链表中没有环。
+
+进阶：
+你是否可以不用额外空间解决此题？
+"""
+class Solution(object):
+    """
+    时间复杂度：O(n)
+    空间复杂度：O(1)
+    """
+    def getIntersect(self, head):
+        tortoise = head
+        hare = head
+
+        # A fast pointer will either loop around a cycle and meet the slow
+        # pointer or reach the `null` at the end of a non-cyclic list.
+        while hare is not None and hare.next is not None:
+            tortoise = tortoise.next
+            hare = hare.next.next
+            if tortoise == hare:
+                return tortoise
+
+        return None
+
+    def detectCycle(self, head):
+        if head is None:
+            return None
+
+        # If there is a cycle, the fast/slow pointers will intersect at some
+        # node. Otherwise, there is no cycle, so we cannot find an e***ance to
+        # a cycle.
+        intersect = self.getIntersect(head)
+        if intersect is None:
+            return None
+
+        # To find the e***ance to the cycle, we have two pointers traverse at
+        # the same speed -- one from the front of the list, and the other from
+        # the point of intersection.
+        ptr1 = head
+        ptr2 = intersect
+        while ptr1 != ptr2:
+            ptr1 = ptr1.next
+            ptr2 = ptr2.next
+
+        return ptr1
+
