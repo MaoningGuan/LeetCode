9c4f0d2b4fbdf037dde7b9ce56a16cf484461fa2
Maoning Guan <1812711281@qq.com>
2020-10-14 17:19:25 +0800

    20. 有效的括号

---
+++ 字节跳动测试开发工程师面试准备/isValid.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+20. 有效的括号
+给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+
+有效字符串需满足：
+
+左括号必须用相同类型的右括号闭合。
+左括号必须以正确的顺序闭合。
+注意空字符串可被认为是有效字符串。
+
+示例 1:
+
+输入: "()"
+输出: true
+示例 2:
+
+输入: "()[]{}"
+输出: true
+示例 3:
+
+输入: "(]"
+输出: false
+示例 4:
+
+输入: "([)]"
+输出: false
+示例 5:
+
+输入: "{[]}"
+输出: true
+"""
+class Solution:
+    def isValid(self, s: str) -> bool:
+        dic = {'{': '}', '[': ']', '(': ')', '?': '?'}
+        stack = ['?']
+        for c in s:
+            print('ok')
+            if c in dic:
+                stack.append(c)
+            elif dic[stack.pop()] != c:
+                return False
+        return len(stack) == 1
+
+
+if __name__ == '__main__':
+    s = ''
+    solution = Solution()
+    print(solution.isValid(s))
