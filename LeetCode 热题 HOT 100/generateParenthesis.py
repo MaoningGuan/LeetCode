bd896e7ae930e30e77e9f5a180b8f9c27fc2f7e2
Maoning Guan <1812711281@qq.com>
2020-10-31 16:06:12 +0800

    22. 括号生成

---
+++ LeetCode 热题 HOT 100/generateParenthesis.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+22. 括号生成
+数字 n 代表生成括号的对数，请你设计一个函数，用于能够生成所有可能的并且 有效的 括号组合。
+示例：
+输入：n = 3
+输出：[
+       "((()))",
+       "(()())",
+       "(())()",
+       "()(())",
+       "()()()"
+     ]
+"""
+from typing import List
+
+
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        """
+        深度优先遍历
+        :param n:
+        :return:
+        """
+        res = []
+        cur_str = ''
+
+        def dfs(cur_str, left, right):
+            """
+            :param cur_str: 从根结点到叶子结点的路径字符串
+            :param left: 左括号还可以使用的个数
+            :param right: 右括号还可以使用的个数
+            :return:
+            """
+            if left == 0 and right == 0:
+                res.append(cur_str)
+                return
+            if right < left:
+                return
+            if left > 0:
+                dfs(cur_str + '(', left - 1, right)
+            if right > 0:
+                dfs(cur_str + ')', left, right - 1)
+
+        dfs(cur_str, n, n)
+        return res
+
+
+if __name__ == '__main__':
+    n = 3
+
+    solution = Solution()
+    print(solution.generateParenthesis(n))
